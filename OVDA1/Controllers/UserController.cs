471488b188705ca7d9de5cf0034ac18b0c0ba3c3
kparent <kparent@X770>
2016-08-16 14:55:05 -0400

    Ajoutez des fichiers projet.

---
+++ OVDA1/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OVDA1.Models;
+using OVDA1.Models.Interfaces;
+using OVDA1.Models.Repository;
+using MongoDB.Driver;
+
+namespace OVDA1.Controllers
+{
+    public class UsersController : ApiController
+    {
+
+        static readonly IUserRepository repository = new UserRepository();
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            var client = new MongoClient();
+            return repository.GetAll();            
+        }
+
+        public User GetUser(int id)
+        {
+            User user = repository.Get(id);
+            if (user == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return user;
+        }
+
+        public HttpResponseMessage PostUser(User user)
+        {
+            user = repository.Add(user);
+            var response = Request.CreateResponse<User>(HttpStatusCode.Created, user);
+            string uri = Url.Link("DefaultApi", new { id = user.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        public void PutUser(int id, User user)
+        {
+            user.Id = id;
+            if (!repository.Update(user))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        public void DeleteUser(int id)
+        {
+            User user = repository.Get(id);
+            if (user == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            repository.Remove(id);
+        }
+    }
+}
---
+++ OVDA1/Models/Repository/UserRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OVDA1.Models.Interfaces;
+using OVDA1.Models;
+
+namespace OVDA1.Models.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private List<User> users = new List<User>();
+        private int _nextId = 1;
+
+        public UserRepository()
+        {
+            Add(new User { Name = "Test1" });
+            Add(new User { Name = "Test2" });
+            Add(new User { Name = "Test3" });
+        }
+ 
+        public IEnumerable<User> GetAll()
+        {
+            return users;
+        }
+
+        public User Get(int id)
+        {
+            return users.Find(u => u.Id == id);
+        }
+
+        public User Add(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException("user");
+            }
+            user.Id = _nextId++;
+            users.Add(user);
+            return user;
+        }
+
+        public void Remove(int id)
+        {
+            users.RemoveAll(u => u.Id == id);
+        }
+
+        public bool Update(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException("user");
+            }
+            int index = users.FindIndex(u => u.Id == user.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            users.RemoveAt(index);
+            users.Add(user);
+            return true;
+        }
+    }
+}
